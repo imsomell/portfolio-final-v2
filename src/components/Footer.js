3dc06d527b266e945141ff68a92a2d36e06743c6
imsomell <macapagal.jumel@gmail.com>
2021-03-16 17:26:59 +0800

    Deploying react app to Github

---
+++ src/components/Footer.js
@@ -0,0 +1,117 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from './Button'
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className='footer-container'>
+            <section className="footer-subscription">
+                <p className="footer-subscription-heading">
+                    Join the Adventure newsletter to recieve our best vacation deals
+               </p>
+                <p className="footer-subscription-text">
+                    You can unsubscribe at anytime.
+               </p>
+                <div className="input-areas">
+                    <form>
+                        <input type="email" name="email" placeholder="Your Email" className="footer-input" />
+                        <Button buttonStyle='btn--outline' >
+                            Subscribe
+                       </Button>
+                    </form>
+                </div>
+            </section>
+            <div className="footer-links">
+                <div className='footer-link-wrapper'>
+                    <div class='footer-link-items'>
+                        <h2>About Us</h2>
+                        <Link to='/sign-up'>How it works</Link>
+                        <Link to='/'>Testimonials</Link>
+                        <Link to='/'>Careers</Link>
+                        <Link to='/'>Investors</Link>
+                        <Link to='/'>Terms of Service</Link>
+                    </div>
+                    <div class='footer-link-items'>
+                        <h2>Contact Us</h2>
+                        <Link to='/'>Contact</Link>
+                        <Link to='/'>Support</Link>
+                        <Link to='/'>Destinations</Link>
+                        <Link to='/'>Sponsorships</Link>
+                    </div>
+                </div>
+                <div className='footer-link-wrapper'>
+                    <div class='footer-link-items'>
+                        <h2>Videos</h2>
+                        <Link to='/'>Submit Video</Link>
+                        <Link to='/'>Ambassadors</Link>
+                        <Link to='/'>Agency</Link>
+                        <Link to='/'>Influencer</Link>
+                    </div>
+                    <div class='footer-link-items'>
+                        <h2>Social Media</h2>
+                        <Link to='/'>Instagram</Link>
+                        <Link to='/'>Facebook</Link>
+                        <Link to='/'>Youtube</Link>
+                        <Link to='/'>Twitter</Link>
+                    </div>
+                </div>
+            </div>
+            <section class='social-media'>
+                <div class='social-media-wrap'>
+                    <div class='footer-logo'>
+                        <Link to='/' className='social-logo'>
+                        Travelkaloka
+                        <i className="fab fa-angellist"></i>
+                        </Link>
+                    </div>
+                    <small class='website-rights'>Travelkaloka © 2021</small>
+                    <div class='social-icons'>
+                        <a 
+                            class='social-icon-link facebook'
+                            href='https://www.facebook.com/j.l.macapagal'
+                            target='_blank'
+                            aria-label='Facebook'
+                        >
+                            <i class='fab fa-facebook-f' />
+                        </a>
+                        <Link
+                            class='social-icon-link instagram'
+                            to='/'
+                            target='_blank'
+                            aria-label='Instagram'
+                        >
+                            <i class='fab fa-instagram' />
+                        </Link>
+                        <Link
+                            class='social-icon-link youtube'
+                            to='/'
+                            target='_blank'
+                            aria-label='Youtube'
+                        >
+                            <i class='fab fa-youtube' />
+                        </Link>
+                        <Link
+                            class='social-icon-link twitter'
+                            to='/'
+                            target='_blank'
+                            aria-label='Twitter'
+                        >
+                            <i class='fab fa-twitter' />
+                        </Link>
+                        <Link
+                            class='social-icon-link twitter'
+                            to='/'
+                            target='_blank'
+                            aria-label='LinkedIn'
+                        >
+                            <i class='fab fa-linkedin' />
+                        </Link>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default Footer;
